ef95cc9abd8f10d547b1988dcf6f74271f9372ed
Admin-ПК\Admin <1>
2017-01-21 01:07:40 +0500

    no message

---
+++ DuplexPagesPrint/PrintingPages.cs
@@ -0,0 +1,53 @@
+﻿namespace DuplexPagesPrint
+{
+    class PrintingPages
+    {
+        public string OddPages { get; private set; }
+        public string EvenPages { get; private set; }
+        int amountPages;
+
+        public PrintingPages(int amountPages)
+        {
+            this.amountPages = amountPages;
+            InitializationProperties(amountPages);
+        }
+
+        void InitializationProperties(int amountPages)
+        {
+            #region
+            //for (int i = 0; i < amountPages; i++)
+            //{
+            //    if (i % 8 < 4)
+            //    {
+            //        OddPages += $"{i + 1}" +
+            //                    $"-{(i + 4 > amountPages ? amountPages : i + 4)}" +
+            //                    $"{(amountPages - i > 8 ? ", " : string.Empty)}";
+            //    }
+            //    else
+            //    {
+            //        EvenPages += $"{i + 1}" +
+            //                     $"-{(i + 4 > amountPages ? amountPages : i + 4)}" +
+            //                     $"{(amountPages - i > 4 ? ", " : string.Empty)}";
+            //    }
+            //    i += 3;
+            //}
+            #endregion
+            for (int i = 0; i < amountPages; i++)
+            {
+                if (i % 8 < 4)
+                {
+                    OddPages += (i + 1).ToString();
+                    OddPages += (i + 1 == amountPages) ? string.Empty : $"-{(i + 4 > amountPages ? amountPages : i + 4)}";
+                    OddPages += $"{(amountPages - i > 8 ? ", " : string.Empty)}";
+                }
+                else
+                {
+                    EvenPages += (i + 1).ToString();
+                    EvenPages += (i + 1 == amountPages) ? string.Empty : $"-{(i + 4 > amountPages ? amountPages : i + 4)}";
+                    EvenPages += $"{(amountPages - i > 8 ? ", " : string.Empty)}";
+                }
+                i += 3;
+            }
+        }
+    }
+}
---
+++ DuplexPagesPrint/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace DuplexPagesPrint
+{
+    class Program
+    {
+        static string file = @"OddEven.txt";
+        static int numberPages;
+
+        static void Main(string[] args)
+        {
+            ReadNumberPages();
+            PrintingPages pp = new PrintingPages(numberPages);
+
+            using (var strm = new StreamWriter(file))
+            {
+                strm.WriteLine(pp.OddPages);
+                strm.WriteLine(string.Empty);
+                strm.WriteLine(pp.EvenPages);
+            }
+
+            OpenInExplorer();
+        }
+
+        private static void ReadNumberPages()
+        {
+            Console.Write("Enter the number of pages: ");
+            try
+            {
+                numberPages = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input");
+                ReadNumberPages();
+            }
+        }
+
+        private static void OpenInExplorer()
+        {
+            new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "notepad.exe",
+                    Arguments = file,
+                }
+            }
+            .Start();
+        }
+    }
+}
